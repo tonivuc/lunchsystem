f425b188bdb61cb16c236515e93683d274c6e84a
Toni Vucic <tonivuc@gmail.com>
2022-06-29 13:14:15 +0300

    Changed API port and added react-bootstrap

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const app = express();
+const port = 5000;
 
 app.use(express.json());
-
 
 app.post('/', (req, res) => {
     res.send(404);
 });
 
 app.use("/api/lunch-wishes", require('./routes/api/lunchWishes'));
-app.listen(3000);
+app.listen(port);
 
