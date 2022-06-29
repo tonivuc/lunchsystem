87fde84b096824c1819762b0cb106a2b05b9c093
Toni Vucic <tonivuc@gmail.com>
2022-06-29 16:01:24 +0300

    Name changes

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -6,15 +6,15 @@ import {
 export default function Nav() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>Lunch wish system</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
+        <Link to="/invoices">Lunch wishes</Link> |{" "}
+        <Link to="/expenses">Order checklist</Link>
         
       </nav>
     </div>
