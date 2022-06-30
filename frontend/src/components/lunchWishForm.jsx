4d572bc96b60c4f731623e80a00ca3dd3edf150b
Toni Vucic <tonivuc@gmail.com>
2022-06-30 13:01:52 +0300

    Half-working form validation

--- frontend/src/components/lunchWishForm.jsx
+++ frontend/src/components/lunchWishForm.jsx
@@ -1,30 +1,58 @@
 
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import Button from 'react-bootstrap/Button';
 
 function LunchWishForm(props) {
+
+    const [validated, setValidated] = useState(false);
+
     const nameInputRef = useRef(null);
     const lunchWishInputRef = useRef(null);
 
-  return (
-      <Form onSubmit={e => props.onSubmit(e, nameInputRef?.current?.value, lunchWishInputRef?.current?.value)}>
+    const submitForm = (event, name, lunchItem) => {
+        const form = event.currentTarget;
+        form.checkValidity()
+        if (isFormValid(name, lunchItem)) {
+            props.onSubmit(event, name, lunchItem);
+            setValidated(true);
+        }
+        else {
+            event.preventDefault();
+            event.stopPropagation();
+            setValidated(false);
+        }
+    }
+
+    const isFormValid = (name, lunchItem) => {
+        return name && lunchItem;
+    }
+
+    return (
+        <Form noValidate validated={validated} onSubmit={e => submitForm(e, nameInputRef?.current?.value, lunchWishInputRef?.current?.value)}>
         <Row>
-          <Col>
-            <Form.Control placeholder="Name" ref={nameInputRef} />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Lunch wish" ref={lunchWishInputRef} />
-          </Col>
-          <Col>
-            <Button variant="primary" type="submit">
-            Submit
-            </Button>
-          </Col>
+            <Col>
+                <Form.Control required placeholder="Name" ref={nameInputRef} />
+                <Form.Control.Feedback type="invalid">
+                Please provide a name.
+                </Form.Control.Feedback>
+            </Col>
+            <Col>
+                <Form.Control required placeholder="Lunch wish" ref={lunchWishInputRef} />
+            </Col>
+            <Col>
+                <Button variant="primary" type="submit">
+                Submit
+                </Button>
+            </Col>
         </Row>
-      </Form>
-  );
+        </Form>
+    );
 }
+
 export default LunchWishForm;
+
+
+
